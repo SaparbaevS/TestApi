6518409222bf1221e9f2ebb9ef403027527673db
Sultan <saparbaevsultan17@gmail.com>
2026-04-15 16:46:36 +0500

    create dabase config

--- fastapi-test/core/config.py
+++ fastapi-test/core/config.py
@@ -1,5 +1,7 @@
 from pydantic import BaseModel
+from pydantic import PostgresDsn
 from pydantic_settings import BaseSettings
+
 
 class RunConfig(BaseModel):
     host: str = "0.0.0.0"
@@ -8,9 +10,19 @@ class RunConfig(BaseModel):
 class ApiPrefix(BaseModel):
     prefix: str = "/api"
 
+
+class DatabaseConfig(BaseModel):
+    url: PostgresDsn
+    echo: bool = False
+    echo_pool: bool = False
+    pool_size: int = 50
+    max_overflow: int = 10
+
+
 class Settings(BaseSettings):
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
+    db: DatabaseConfig
 
 
 
