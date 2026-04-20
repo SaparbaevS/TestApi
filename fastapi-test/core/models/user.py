ff4d1b6cb7fd9feab051dbcdeffbd534f2628f72
Sultan <saparbaevsultan17@gmail.com>
2026-04-20 13:38:33 +0500

    create UserIdType var

--- fastapi-test/core/models/user.py
+++ fastapi-test/core/models/user.py
@@ -5,11 +5,12 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from .base import Base
 from .mixins.int_id_pk import IntIdPkMixin
+from core.types.user_id import UserIdType
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
 
-class User(Base, IntIdPkMixin, SQLAlchemyBaseUserTable[int]):
+class User(Base, IntIdPkMixin, SQLAlchemyBaseUserTable[UserIdType]):
     @classmethod
     def get_db(cls, session: "AsyncSession"):
         return SQLAlchemyUserDatabase(session, User)
