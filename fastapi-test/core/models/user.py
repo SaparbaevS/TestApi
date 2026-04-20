4b1be405257a1dfe966a51bcf2edf95c072405d6
Sultan <saparbaevsultan17@gmail.com>
2026-04-20 13:16:55 +0500

    add method for user database

--- fastapi-test/core/models/user.py
+++ fastapi-test/core/models/user.py
@@ -1,16 +1,16 @@
-from sqlalchemy import UniqueConstraint
-from sqlalchemy.orm import Mapped, mapped_column
+from typing import TYPE_CHECKING
+
+from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from .base import Base
 from .mixins.int_id_pk import IntIdPkMixin
 
+if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
 
-
-class User(IntIdPkMixin , Base):
-    username: Mapped[str] = mapped_column(unique=True)
-    foo: Mapped[int]
-    bar: Mapped[int]
-
-    __table_args__ = (
-        UniqueConstraint("foo", "bar"),
-    )
+class User(Base, IntIdPkMixin, SQLAlchemyBaseUserTable[int]):
+    @classmethod
+    def get_db(cls, session: "AsyncSession"):
+        return SQLAlchemyUserDatabase(session, User)
     
