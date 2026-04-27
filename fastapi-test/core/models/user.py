b86feaf24d6459674933d26c7a3012b0ae9f2870
Sultan <saparbaevsultan17@gmail.com>
2026-04-27 15:38:06 +0500

    add get_users method to

--- fastapi-test/core/models/user.py
+++ fastapi-test/core/models/user.py
@@ -1,17 +1,30 @@
 from typing import TYPE_CHECKING
 
-from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
+from fastapi_users_db_sqlalchemy import (
+    SQLAlchemyBaseUserTable,
+    SQLAlchemyUserDatabase as SQLAlchemyUserDatabaseGeneric,
+)
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from .base import Base
 from .mixins.int_id_pk import IntIdPkMixin
 from core.types.user_id import UserIdType
+from sqlalchemy import select
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
+
+
+class SQLAlchemyUserDatabase(SQLAlchemyUserDatabaseGeneric):
+    
+    async def get_users(self) -> list["User"]:
+        statement = select(User).order_by(User.id)
+        results = await self.session.scalars(statement)
+        return list(results.all())
+    
+
 
 class User(Base, IntIdPkMixin, SQLAlchemyBaseUserTable[UserIdType]):
     @classmethod
     def get_db(cls, session: "AsyncSession"):
         return SQLAlchemyUserDatabase(session, cls)
-    
