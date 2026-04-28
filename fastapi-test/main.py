76dd141f12f03c5d70eb75a1b00e3a7d6e29182c
Sultan <saparbaevsultan17@gmail.com>
2026-04-28 15:14:33 +0500

    add middleware

--- fastapi-test/main.py
+++ fastapi-test/main.py
@@ -1,15 +1,31 @@
+import logging
+import time
 from contextlib import asynccontextmanager
+from typing import Callable, Awaitable
 
-from fastapi import FastAPI
+
+from fastapi import FastAPI, Request, Response
 from redis.asyncio import Redis
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
+from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.base import BaseHTTPMiddleware
+
 
 # from actions.create_superuser import create_superuser
 from api import router as api_router
 from core.models import db_helper
 from core.config import settings
 
+
+log = logging.getLogger(__name__)
+
+ALLOW_ORIGINS = [
+    "http://localhost",
+    "http://localhost:8000",
+]
+
+CallNext = Callable[[Request], Awaitable[Response]]
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -33,6 +49,57 @@ main_app = FastAPI(
     lifespan=lifespan,
 )
 main_app.include_router(api_router)
+
+async def add_process_time_to_requests(
+    request: Request,
+    call_next: CallNext,
+) -> Response:
+    start_time = time.perf_counter()
+    response = await call_next(request)
+    process_time = time.perf_counter() - start_time
+    response.headers["X-Process-Time"] = f"{process_time:.5f}"
+    return response
+
+class ProcessTimeHeaderMiddleware(BaseHTTPMiddleware):
+    def __init__(self, *args, process_time_header_name: str, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.header_name = process_time_header_name
+
+    async def dispatch(
+        self,
+        request: Request,
+        call_next: CallNext,
+    ) -> Response:
+        start_time = time.perf_counter()
+        response = await call_next(request)
+        process_time = time.perf_counter() - start_time
+        response.headers[self.header_name] = f"{process_time:.5f}"
+        return response
+
+
+@main_app.middleware("http")
+async def log_new_request(
+    request: Request,
+    call_next: Callable[[Request], Awaitable[Response]],
+):
+    log.info(
+        "Request %s to %s",
+        request.method,
+        request.url,
+    )
+    return await call_next(request)
+
+main_app.middleware("http")(add_process_time_to_requests)
+main_app.add_middleware(
+    CORSMiddleware,
+    allow_origins=ALLOW_ORIGINS,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+main_app.add_middleware(
+    ProcessTimeHeaderMiddleware,
+    process_time_header_name="X-Process-Time-New-Again",
+)
 
 
 if __name__ == "__main__":
